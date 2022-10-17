4736fe4c93653e8ace72036834ff0121d7f650bc
vivekky <vivekky57@gmail.com>
2022-10-18 04:20:21 +0530

    Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,37 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('', (req, res) => {
-    res.send('Hello express!')
-})
+app.get("", (req, res) => {
+  res.send("<h1> Weather</h1>");
+});
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
-})
-
-app.get('/about', (req, res) => {
-    res.send('About')
-})
-
-app.get('/weather', (req, res) => {
-    res.send('Your weather')
-})
+app.get("/help", (req, res) => {
+  res.send({
+    name: "vivek",
+    age: 25,
+  });
+});
+app.get("/about", (req, res) => {
+  res.send([
+    {
+      name: "Sara",
+    },
+    {
+      name: "vivek",
+    },
+  ]);
+});
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
+});
+//app.com
+//app.com/help
+//app.com/about
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
-})
+  console.log("Server is up on port 3000");
+});
