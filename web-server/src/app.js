f4f932650a695a6cab2a5d6df6999322918efac4
vivekky <vivekky57@gmail.com>
2022-10-18 06:03:47 +0530

    47 Dynamic Pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,36 +1,51 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
-app.get("/help", (req, res) => {
-  res.send({
-    name: "vivek",
-    age: 25,
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Vivek Yadav",
   });
 });
+
 app.get("/about", (req, res) => {
-  res.send([
-    {
-      name: "Sara",
-    },
-    {
-      name: "vivek",
-    },
-  ]);
+  res.render("about", {
+    title: "About Me",
+    name: "Andrew Mead",
+  });
 });
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    name: "Vivek Yadav",
+  });
+});
+// app.get("/about", (req, res) => {
+//   res.send([
+//     {
+//       name: "Sara",
+//     },
+//     {
+//       name: "vivek",
+//     },
+//   ]);
+// });
 app.get("/weather", (req, res) => {
   res.send({
     forecast: "It is snowing",
     location: "Philadelphia",
   });
 });
-//app.com
-//app.com/help
-//app.com/about
+// app.com
+// app.com/help
+// app.com/about
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
