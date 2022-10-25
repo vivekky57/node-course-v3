1adc14ec2ff93de3b63e10b44549050bb2f5dd1e
vivekky <vivekky57@gmail.com>
2022-10-26 03:42:56 +0530

    creating a Mongoose model and data validation and sanitization

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  //   useCreateIndex: true,
+});
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validator(value) {
+      if (!this.validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be postive Number");
+      }
+    },
+  },
+});
+
+const me = new User({
+  name: "Vivek",
+  email: "MYEMAIL@MEAD.IO",
+  age: 6,
+});
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log("Error!", error);
+  });
+
+// const Task = mongoose.model("Task", {
+//   description: {
+//     type: String,
+//   },
+//   completed: {
+//     type: Boolean,
+//   },
+// });
+
+// const task = new Task({
+//   description: "Learn Mongoose Library",
+//   completed: false,
+// });
+
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
