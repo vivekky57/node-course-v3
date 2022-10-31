13e43eefb6b97f15cd1b3eac9016ab8244810a63
vivekky <vivekky57@gmail.com>
2022-10-31 16:11:29 +0530

    Section 11 Resource Creation Endpoints

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -5,56 +5,19 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   //   useCreateIndex: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validator(value) {
-      if (!this.validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be postive Number");
-      }
-    },
-  },
-});
-
-const me = new User({
-  name: "Vivek",
-  email: "MYEMAIL@MEAD.IO",
-  age: 6,
-});
-
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log("Error!", error);
-  });
-
-// const Task = mongoose.model("Task", {
-//   description: {
-//     type: String,
-//   },
-//   completed: {
-//     type: Boolean,
-//   },
+// const me = new User({
+//   name: "Vivek",
+//   email: "MYEMAIL@MEAD.IO",
+//   age: 6,
 // });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log("Error!", error);
+//   });
 
 // const task = new Task({
 //   description: "Learn Mongoose Library",
