f8ee07ebc66d98f743f2d0854483ef8ec8f10167
vivekky <vivekky57@gmail.com>
2022-11-01 20:50:05 +0530

    Section 11 Resource Updating and Deleting Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,131 +1,14 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
-const { MongoUnexpectedServerResponseError } = require("mongodb");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-  // user
-  //   .save()
-  //   .then(() => {
-  //     res.send(user);
-  //   })
-  //   .catch((e) => {
-  //     // console.log(req.body);
-  //     res.status(400).send(e);
-  //   });
-
-  // console.log(req.body);
-  // res.send("testing!");
-});
-
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-  // task
-  //   .save()
-  //   .then(() => {
-  //     res.status(201).send(task);
-  //   })
-  //   .catch((e) => {
-  //     res.status(400).send(e);
-  //   });
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.status(201).send(users);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-  // User.find({})
-  //   .then((users) => {
-  //     res.send(users);
-  //   })
-  //   .catch((e) => {
-  //     res.status(500).send();
-  //   });
-});
-
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-  // User.findById(_id)
-  //   .then((user) => {
-  //     if (!user) {
-  //       return res.status(404).send();
-  //     }
-  //     res.send(user);
-  //   })
-  //   .catch((e) => {
-  //     res.status(500).send();
-  //   });
-});
-
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(201).send(tasks);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-  // Task.find({})
-  //   .then((tasks) => {
-  //     res.send(tasks);
-  //   })
-  //   .catch((e) => {
-  //     res.status(500).send();
-  //   });
-});
-
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-  // Task.findById(_id)
-  //   .then((task) => {
-  //     if (!task) {
-  //       return res.status(404).send();
-  //     }
-
-  //     res.send(task);
-  //   })
-  //   .catch((e) => {
-  //     res.status(500).send(e);
-  //   });
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port" + port);
