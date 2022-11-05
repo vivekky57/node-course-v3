a305f3ad97cd74aebe8c221b0aadedb6e69537f1
vivekky <vivekky57@gmail.com>
2022-11-05 18:50:24 +0530

    Section 12 Accepting Auth Token.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,8 +5,20 @@ const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.json());
 
+// app.use((req, res, next) => {
+//   if (req.method == "GET") {
+//     res.send("GET requests are disbled");
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send("site is currently down. check back soon!");
+// });
+
+app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
@@ -14,16 +26,16 @@ app.listen(port, () => {
   console.log("Server is up on port" + port);
 });
 
-const jwt = require("jsonwebtoken");
-const myFunction = async () => {
-  const token = jwt.sign({ _id: "abc123" }, "thisismynewcourse", {
-    expiresIn: "7d",
-  });
-  console.log(token);
+// const jwt = require("jsonwebtoken");
+// const myFunction = async () => {
+//   const token = jwt.sign({ _id: "abc123" }, "thisismynewcourse", {
+//     expiresIn: "7d",
+//   });
+//   console.log(token);
 
-  const data = jwt.verify(token, "thisismynewcourse");
-  console.log(data);
-};
+//   const data = jwt.verify(token, "thisismynewcourse");
+//   console.log(data);
+// };
 
 // const bcrypt = require("bcryptjs");
 
@@ -36,4 +48,4 @@ const myFunction = async () => {
 //   const isMatch = await bcrypt.compare("Red12345!", hashedPassword);
 //   console.log(isMatch);
 // };
-myFunction();
+// myFunction();
