0c28f35924b0b99a99ac8e45067a52b42ded5a52
vivekky <vivekky57@gmail.com>
2022-11-05 17:46:52 +0530

    Section 12 Generating Auth Token.

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -6,7 +6,8 @@ router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -22,6 +23,19 @@ router.post("/users", async (req, res) => {
 
   // console.log(req.body);
   // res.send("testing!");
+});
+
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.status(200).send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 router.get("/users", async (req, res) => {
