2cc8799f69c26e98d934c86f3c06cf0d434f1b6f
vivekky <vivekky57@gmail.com>
2022-11-05 22:39:48 +0530

    Section 12 logging out

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -38,6 +38,25 @@ router.post("/users/login", async (req, res) => {
     res.status(400).send(e);
   }
 });
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
